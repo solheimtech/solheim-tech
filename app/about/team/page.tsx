1da094dee222996852102ad55f3a1b6afddda295
David Solheim <david@solheimtech.com>
2024-06-06 17:19:27 -0700

    Change Our Team to Team

---
+++ app/about/team/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const OurTeam = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>Meet the dedicated team behind Solheim Technologies.</p>
+    </div>
+  );
+};
+
+export default OurTeam;
