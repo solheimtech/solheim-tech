3a675a57c2da302c7a71fc216ca1350c4fd91567
tateejt <etate.dev@gmail.com>
2024-06-10 11:32:00 -0700

    Created teams page and team-card component

--- app/about/team/page.tsx
+++ app/about/team/page.tsx
@@ -1,12 +1,43 @@
 "use client";
 
 import React from "react";
+import Card from "../../components/ui/team-card";
 
 const OurTeam = () => {
+  const users = [
+    {
+      name: "David Solheim",
+      image: "https://solheimtech.com/wp-content/uploads/2022/11/David.jpg",
+      role: "Chief Executive Officer",
+      description: "David built his first computer when he was eight years old. He built his first website at fourteen. In 2012 he founded Solheim Technologies with the aim of helping small businesses get the websites they need to thrive in the digital age. Since then he has built numerous websites, provided website hosting services, and provided peace of mind for small business owners by managing their websites. Recently he wanted to promote Solheim Technologies services in the other areas of his expertise and provide computer and technology support to both small businesses and residential clients."
+    },
+    {
+      name: "Angela Solheim",
+      image: "https://solheimtech.com/wp-content/uploads/2022/11/Angela_Solheim-x-600.jpg",
+      role: "Chief Financial Officer",
+      description: "Angela has been a part of Solheim Technologies from the beginning when it was just her and David. Because of this she also has learned how to support clients technological needs. Over time as the company has grown, Angela has taken on a role that involves more and more financial responsibilities for the company."
+    },
+    {
+      name: "Soren Reber",
+      image: "https://solheimtech.com/wp-content/uploads/2022/11/Soren.jpg",
+      role: "Head of Technology",
+      description: "Soren has always loved computers but didn’t discover his love for working on them until high school where he took classes refurbishing donated computers. While not initially following up on that passion, he eventually got a degree where he learned more about computer support, network design and management, and even a little coding. As a technology specialist he is helping with clients computer and website needs."
+    },
+    // Add more user objects here
+  ];
+
   return (
     <div>
-      <h1>Our Team</h1>
-      <p>Meet the dedicated team behind Solheim Technologies.</p>
+      <div className="flex flex-col items-center justify-start min-h-[4rem] pt-8 text-white pb-8">
+        <h1 className="text-[2rem] font-bold">Meet the Team</h1>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center md:space-x-8 overflow-x-auto p-4">
+        {users.map((user, index) => (
+          <div key={index} className="p-2">
+            <Card user={user} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ app/components/ui/team-card.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import { FC, useState } from "react";
+import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+interface User {
+  name: string;
+  image: string;
+  role: string;
+  description: string; // Added description field
+}
+
+interface CardProps {
+  user: User
+}
+
+const Card: FC<CardProps> = ({ user }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleCardClick = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <motion.div
+    //className={`bg-white border border-gray-200 rounded-2xl shadow-lg overflow-hidden flex flex-col items-center transform transition-transform duration-300 ${isExpanded ? 'max-w-3xl' : 'max-w-2xl'} sm:flex-row`}
+
+      className={`bg-white border border-gray-200 rounded-2xl shadow-lg overflow-hidden flex flex-col items-center transform transition-transform duration-300 ${isExpanded ? 'max-w-3xl' : 'max-w-2xl'}`}
+      onClick={handleCardClick}
+      animate={{ scale: isExpanded ? 1.1 : 1 }}
+      transition={{ duration: 0.3 }}
+      whileHover={{ scale: 1.05, cursor: 'pointer' }}
+    >
+      <Image
+        className="w-64 h-64 object-cover m-4"
+        src={user.image}
+        alt={user.name}
+        width={256}
+        height={256}
+      />
+      <div className="p-6 w-full">
+        <div className="flex flex-col items-start mb-4">
+          <h5 className="text-2xl font-bold text-gray-900">{user.name}</h5>
+        </div>
+        <div className="border-t border-gray-200 pt-4 w-full text-left">
+          <div className="mb-2">
+            <span className="block text-sm text-gray-500">Role</span>
+            <span className="block text-lg text-gray-900">{user.role}</span>
+          </div>
+        </div>
+        {isExpanded && (
+          <div className="mt-4 text-gray-700">
+            <p>{user.description}</p>
+          </div>
+        )}
+        <div className="flex justify-start items-center mt-6 space-x-6">
+          <motion.a
+            href="#"
+            className="opacity-20 hover:text-black hover:opacity-100 transition-colors duration-300"
+            whileHover={{ scale: 1.1 }}
+          >
+            <FaFacebook size={28} />
+          </motion.a>
+          <motion.a
+            href="#"
+            className="opacity-20 hover:text-black hover:opacity-100 transition-colors duration-300"
+            whileHover={{ scale: 1.1 }}
+          >
+            <FaTwitter size={28} />
+          </motion.a>
+          <motion.a
+            href="#"
+            className="opacity-20 hover:text-black hover:opacity-100 transition-colors duration-300"
+            whileHover={{ scale: 1.1 }}
+          >
+            <FaLinkedin size={28} />
+          </motion.a>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Card;
+
+
