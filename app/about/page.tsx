fdc1c0720f1046358729155c3d571d3b75a88027
tateejt <etate.dev@gmail.com>
2024-06-09 20:29:46 -0700

    Made Changes to About page added review section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,21 +3,55 @@
 import { NextPage } from "next";
 import React, { useRef } from "react";
 import { Meteors } from "../components/ui/meteors";
+import { InfiniteMovingCards } from "../components/ui/infinite-moving-cards";
+
+
+const testimonials = [
+  {
+    quote:
+      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+    name: "Charles Dickens",
+    title: "A Tale of Two Cities",
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: "William Shakespeare",
+    title: "Hamlet",
+  },
+  {
+    quote: "All that we see or seem is but a dream within a dream.",
+    name: "Edgar Allan Poe",
+    title: "A Dream Within a Dream",
+  },
+  {
+    quote:
+      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+    name: "Jane Austen",
+    title: "Pride and Prejudice",
+  },
+  {
+    quote:
+      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    name: "Herman Melville",
+    title: "Moby-Dick",
+  },
+];
+
 
 const ServicesCard: React.FC = () => {
-  const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   return (
-    <div className="flex flex-col items-center gap-[8rem]">
-      <h1 className="text-4xl text-white font-bold mb-2">Services</h1>
-      <div className="flex flex-wrap justify-center gap-[8rem]">
+    <div className="flex flex-col items-center gap-[4rem]">
+      <h1 className="text-3xl text-white font-bold">Services</h1>
+      <div className="flex flex-wrap justify-center gap-[4rem]">
         {[1, 2, 3].map((_, index) => (
           <div key={index} className="w-full sm:w-auto">
             <div
               className="w-full relative max-w-xs mx-auto transition-transform duration-300">
-              <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-gray-800 to-gray-900 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
-              <div className="relative shadow-xl bg-black border border-gray-800 px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
-                <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-900">
+              <div className="absolute inset-0 h-full w-full bg-gradient-to-r  transform scale-[0.80]  rounded-full blur-3xl" />
+              <div className="relative shadow-xl bg-transparent border border-gray-900 px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+                <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-none">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -63,14 +97,24 @@ const AboutUs: NextPage = () => {
   return (
     <div className="relative min-h-screen">
       <div className="flex flex-col items-center justify-start min-h-screen pt-8 relative z-10">
-        <h1 className="text-4xl text-white font-bold">About Us</h1>
+        <h1 className="text-3xl text-white font-bold">About Us</h1>
         <div className="flex flex-col items-center justify-center min-h-screen">
           {/* <Image src="/meteor.png" alt="Meteor" width={100} height={100} /> */}
         </div>
       </div>
+
       <div className="relative z-10">
         <ServicesCard />
       </div>
+
+      <div className="h-[40rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
+        <p className="text-white text-3xl font-bold pb-5">Customer Review</p>
+      <InfiniteMovingCards
+        items={testimonials}
+        direction="right"
+        speed="slow"
+      />
+    </div>
     </div>
   );
 };
