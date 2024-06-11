23a3ef3887f6231ab8fcf59583adb6cabfe13b2a
tateejt <etate.dev@gmail.com>
2024-06-10 17:10:35 -0700

    Added state variables

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,26 +1,40 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
+import { CiCircleCheck } from "react-icons/ci";
+import { GoXCircle } from "react-icons/go";
+import Script from 'next/script';
 
 const ContactPage = () => {
-  const handleBlur = (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+
+const [email, setEmail] = useState('');
+const [phone, setPhone] = useState('');
+const [firstName, setFirstName] = useState('');
+const [lastName, setLastName] = useState('');
+const [company, setCompany] = useState('');
+const [message, setMessage] = useState('');
+const [status, setStatus] = useState('');
+
+
+const handleBlur = (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const field = e.target;
     const errorElement = field.nextElementSibling as HTMLElement;
     if (field.hasAttribute('required') && !field.value.trim()) {
-      if (errorElement && field.previousElementSibling) {
-        const labelText = (field.previousElementSibling as HTMLElement).textContent?.replace('*', '').trim();
-        errorElement.textContent = `${labelText} is required.`;
-        field.classList.add('focus:ring-red-500');
-        field.classList.add('focus:border-red-500');
-      }
+        if (errorElement && field.previousElementSibling) {
+            const labelText = (field.previousElementSibling as HTMLElement).textContent?.replace('*', '').trim();
+            errorElement.textContent = `${labelText} is required.`;
+            field.classList.add('focus:ring-red-500');
+            field.classList.add('focus:border-red-500');
+        }
     } else {
-      if (errorElement) {
-        errorElement.textContent = "";
-        field.classList.remove('focus:ring-red-500');
-        field.classList.remove('focus:border-red-500');
-      }
+        if (errorElement) {
+            errorElement.textContent = "";
+            field.classList.remove('focus:ring-red-500');
+            field.classList.remove('focus:border-red-500');
+        }
     }
-  };
+};
+
 
   return (
     <div className="container mx-auto px-4 py-8 md:px-6 text-white">
@@ -74,42 +88,41 @@ const ContactPage = () => {
             });
 
             if (allFieldsValid) {
-              // Handle form submission
-              console.log("Form submitted successfully");
+              console.log('Form submitted successfully!');
             }
           }}>
             <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
               <div className="w-full md:w-1/2">
                 <label htmlFor="first-name" className="block text-sm font-medium text-black">First Name <span className="text-red-500">*</span></label>
-                <input type="text" id="first-name" name="first-name" className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required onBlur={handleBlur} />
+                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} id="first-name" name="first-name" className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required onBlur={handleBlur} />
                 <p className="text-red-500 text-sm mt-1"></p>
               </div>
               <div className="w-full md:w-1/2">
                 <label htmlFor="last-name" className="block text-sm font-medium text-black">Last Name <span className="text-red-500">*</span></label>
-                <input type="text" id="last-name" name="last-name" className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required onBlur={handleBlur} />
+                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} id="last-name" name="last-name" className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required onBlur={handleBlur} />
                 <p className="text-red-500 text-sm mt-1"></p>
               </div>
             </div>
             <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
               <div className="w-full md:w-1/2">
                 <label htmlFor="email" className="block text-sm font-medium text-black">Email Address <span className="text-red-500">*</span></label>
-                <input type="email" id="email" name="email" className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required onBlur={handleBlur} />
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" name="email" className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required onBlur={handleBlur} />
                 <p className="text-red-500 text-sm mt-1"></p>
               </div>
               <div className="w-full md:w-1/2">
                 <label htmlFor="phone" className="block text-sm font-medium text-black">Phone Number <span className="text-red-500">*</span></label>
-                <input type="tel" id="phone" name="phone" className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required onBlur={handleBlur} />
+                <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} id="phone" name="phone" className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required onBlur={handleBlur} />
                 <p className="text-red-500 text-sm mt-1"></p>
               </div>
             </div>
             <div className="w-full">
               <label htmlFor="company" className="block text-sm font-medium text-black">Company Name</label>
-              <input type="text" id="company" name="company" className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" onBlur={handleBlur} />
+              <input type="text" value={company} onChange={(e) => setCompany(e.target.value)} id="company" name="company" className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" onBlur={handleBlur} />
               <p className="text-red-500 text-sm mt-1 hidden"></p>
             </div>
             <div>
               <label htmlFor="message" className="block text-sm font-medium text-black">Message <span className="text-red-500">*</span></label>
-              <textarea id="message" name="message" rows={4} className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required onBlur={handleBlur}></textarea>
+              <textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} name="message" rows={4} className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required onBlur={handleBlur}></textarea>
               <p className="text-red-500 text-sm mt-1"></p>
             </div>
             
@@ -118,6 +131,26 @@ const ContactPage = () => {
                 Send Message
               </button>
             </div>
+            {status && status !== '' && (
+              <div className="fixed inset-0 flex items-center justify-center z-50">
+                <div className="bg-white p-6 rounded shadow-lg text-black">
+                  <div className="flex items-center">
+                    {status === 'Form submitted successfully!' ? (
+                      <CiCircleCheck className="w-6 h-6 text-green-500" />
+                    ) : (
+                      <GoXCircle className="w-6 h-6 text-red-500" />
+                    )}
+                    <p className="ml-2">{status}</p>
+                  </div>
+                  <button
+                    onClick={() => setStatus('')}
+                    className="mt-4 px-4 py-2 text-black bg-white border-black border hover:bg-black hover:text-white rounded"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            )}
           </form>
         </div>
       </div>
