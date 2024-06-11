405586f2011d4d4e8bf09f7322466af882d9a66e
tateejt <etate.dev@gmail.com>
2024-06-10 17:09:41 -0700

    Updated team cards to handle expanding one card at a time

--- app/about/team/page.tsx
+++ app/about/team/page.tsx
@@ -1,27 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../components/ui/team-card";
 
 const OurTeam = () => {
+  const [expandedUser, setExpandedUser] = useState<string | null>(null);
+
   const users = [
     {
       name: "David Solheim",
       image: "https://solheimtech.com/wp-content/uploads/2022/11/David.jpg",
       role: "Chief Executive Officer",
-      description: "David built his first computer when he was eight years old. He built his first website at fourteen. In 2012 he founded Solheim Technologies with the aim of helping small businesses get the websites they need to thrive in the digital age. Since then he has built numerous websites, provided website hosting services, and provided peace of mind for small business owners by managing their websites. Recently he wanted to promote Solheim Technologies services in the other areas of his expertise and provide computer and technology support to both small businesses and residential clients."
+      description: "David built his first computer when he was eight years old. He built his first website at fourteen. In 2012 he founded Solheim Technologies with the aim of helping small businesses get the websites they need to thrive in the digital age. Since then he has built numerous websites, provided website hosting services, and provided peace of mind for small business owners by managing their websites. Recently he wanted to promote Solheim Technologies services in the other areas of his expertise and provide computer and technology support to both small businesses and residential clients.",
+      facebook: "https://www.facebook.com/david.solheim.7",
+      twitter: "https://twitter.com/david_solheim",
+      linkedin: "https://www.linkedin.com/in/david-solheim-7b3b1b1b1/"
     },
     {
       name: "Angela Solheim",
       image: "https://solheimtech.com/wp-content/uploads/2022/11/Angela_Solheim-x-600.jpg",
       role: "Chief Financial Officer",
-      description: "Angela has been a part of Solheim Technologies from the beginning when it was just her and David. Because of this she also has learned how to support clients technological needs. Over time as the company has grown, Angela has taken on a role that involves more and more financial responsibilities for the company."
+      description: "Angela has been a part of Solheim Technologies from the beginning when it was just her and David. Because of this she also has learned how to support clients technological needs. Over time as the company has grown, Angela has taken on a role that involves more and more financial responsibilities for the company.",
+      facebook: "https://www.facebook.com/angela.solheim.7",
+      twitter: "https://twitter.com/angela_solheim",
+      linkedin: "https://www.linkedin.com/in/angela-solheim-7b3b1b1b1/"
     },
     {
       name: "Soren Reber",
       image: "https://solheimtech.com/wp-content/uploads/2022/11/Soren.jpg",
       role: "Head of Technology",
-      description: "Soren has always loved computers but didn’t discover his love for working on them until high school where he took classes refurbishing donated computers. While not initially following up on that passion, he eventually got a degree where he learned more about computer support, network design and management, and even a little coding. As a technology specialist he is helping with clients computer and website needs."
+      description: "Soren has always loved computers but didn’t discover his love for working on them until high school where he took classes refurbishing donated computers. While not initially following up on that passion, he eventually got a degree where he learned more about computer support, network design and management, and even a little coding. As a technology specialist he is helping with clients computer and website needs.",
+      facebook: "https://www.facebook.com/soren.reber.7",
+      twitter: "https://twitter.com/soren_reber",
+      linkedin: "https://www.linkedin.com/in/soren-reber-7b3b1b1b1/"
     },
     // Add more user objects here
   ];
@@ -34,7 +45,7 @@ const OurTeam = () => {
       <div className="flex flex-col md:flex-row justify-center md:space-x-8 overflow-x-auto p-4">
         {users.map((user, index) => (
           <div key={index} className="p-2">
-            <Card user={user} />
+            <Card user={user} expandedUser={expandedUser} onExpand={setExpandedUser} />
           </div>
         ))}
       </div>
--- app/components/ui/infinite-moving-cards.tsx
+++ app/components/ui/infinite-moving-cards.tsx
@@ -10,9 +10,9 @@ export const InfiniteMovingCards = ({
   className,
 }: {
   items: {
-    quote: string;
+    description: string;
     name: string;
-    title: string;
+    rating: number; // Add rating property
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
@@ -97,15 +97,26 @@ export const InfiniteMovingCards = ({
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
               <span className="relative z-20 text-sm leading-[1.6] text-black font-normal">
-                {item.quote}
+                {item.description}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
                   <span className="text-sm leading-[1.6] text-gray-800 font-normal">
                     {item.name}
                   </span>
-                  <span className="text-sm leading-[1.6] text-gray-800 font-normal">
-                    {item.title}
+                  <span className="flex">
+                    {Array.from({ length: 5 }, (_, i) => (
+                      <svg
+                        key={i}
+                        className={`w-4 h-4 ${
+                          i < item.rating ? "text-yellow-500" : "text-gray-300"
+                        }`}
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.97a1 1 0 00.95.69h4.18c.969 0 1.371 1.24.588 1.81l-3.388 2.46a1 1 0 00-.364 1.118l1.287 3.97c.3.921-.755 1.688-1.54 1.118l-3.388-2.46a1 1 0 00-1.175 0l-3.388 2.46c-.784.57-1.838-.197-1.54-1.118l1.287-3.97a1 1 0 00-.364-1.118L2.34 9.397c-.783-.57-.38-1.81.588-1.81h4.18a1 1 0 00.95-.69l1.286-3.97z" />
+                      </svg>
+                    ))}
                   </span>
                 </span>
               </div>
