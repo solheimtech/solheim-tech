21b005f10f0e9e99a454bb3dc5785fe1c374a332
tateejt <etate.dev@gmail.com>
2024-06-12 13:16:08 -0700

    adjusted services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default function Services() {
-  return <div>Services</div>;
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Services</h1>
+    </div>
+  );
 }
 
