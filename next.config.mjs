4088f1a23267f3b0f1c751f5ee630e867db189d5
Soren Reber <78244444+sorenReber@users.noreply.github.com>
2024-06-21 09:15:53 -0700

    Added a redirect for the Complete Calibrations Review NFC

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     domains: ['assets.aceternity.com', 'solheimtech.com'],
   },
+  async redirects() {
+    return [
+      // Redirects for /our-team/*
+      {
+        source: '/nfc/clients/complete-calibrations/mesa/google-review',
+        destination: 'https://g.page/r/Caxsrxx5LDDbEAI/review',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
