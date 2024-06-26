793a28238dd2f9c34870d41882cd9f8fdac8e796
David Solheim <david@solheimtech.com>
2024-06-26 12:36:19 -0700

    Updated padding and links on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -42,8 +42,8 @@ export default function Home() {
 
   return (
     <main className="flex-1">
-        <section className="w-full h-screen py-12 md:py-24 lg:py-32">
-          <div className="container h-full px-4 md:px-6">
+        <section className="w-full h-auto py-12 pt-16 md:py-24 lg:py-32">
+          <div className="container mx-auto h-full px-4 md:px-6">
             <div className="grid h-full gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
               <div className="flex flex-col justify-center space-y-4 h-full">
                 <div className="space-y-2 animate-fade-in">
@@ -57,14 +57,14 @@ export default function Home() {
                 </div>
                 <div className="flex flex-col gap-2 min-[400px]:flex-row animate-fade-in-up">
                   <Link
-                    href="#"
+                    href="/meeting/free-consultation"
                     className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                     prefetch={false}
                   >
                     Get Started
                   </Link>
                   <Link
-                    href="#"
+                    href="/services"
                     className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
                     prefetch={false}
                   >
