13959298f2153cffceb0ae32d9709c6392461896
Emery Tate <etate.dev@gmail.com>
2024-07-11 09:46:40 -0700

    remove searchparams

--- app/components/LayoutWrapper.tsx
+++ app/components/LayoutWrapper.tsx
@@ -1,20 +1,19 @@
 "use client";
 
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return (
     <motion.div
-      key={pathname + searchParams.toString()}
+      key={pathname}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
